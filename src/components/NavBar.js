597ab9c5ee42e7c3460c2d1390e09838f9ca5ee5
Chun Yi <8.5by11@gmail.com>
2022-04-10 12:33:17 -0400

    Conditional rendering of Navbar and dropdown

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,62 +1,80 @@
-
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
-const NavBar = ({ currentUser }) => {
-
+//TODO add logo
+function NavBar({ currentUser }) {
   return (
-        <div>
-            {/* <Navbar fixed="top" collapseOnSelect expand="md" bg="dark" variant="dark"> */}
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <Container>
+	<Navbar bg='dark' variant='dark' fixed='top' collapseOnSelect expand='lg'>
+		<Container>
+			<Navbar.Brand href='/'>🍿 Watchables
+				{/* <img
+					src='https://c.tenor.com/88dnH_mHRLAAAAAC/static-tv-static.gif'
+					width='30'
+					height='30'
+					className='d-inline-block align-top'
+					alt='watchables logo'
+				/> */}
+			</Navbar.Brand>
+			<Navbar.Toggle aria-controls='responsive-navbar-nav' />
+			<Navbar.Collapse id='responsive-navbar-nav'>
 
-                <Navbar.Brand href="/">🍿 Watchables List</Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="/watchables-list">Watchables List</Nav.Link>
-                        <Nav.Link href="/add">Add a Watchable</Nav.Link>
-                        <Nav.Link href="/favorites">View Favorites</Nav.Link>
-                    </Nav>
+				<Nav className='me-auto'>
+					<Nav.Link href='/watchables-list'>Watchables List</Nav.Link>
+					{
+						currentUser
+						?
+						<Nav.Link href='/add'>Add a Watchable</Nav.Link>
+						:
+						null
+					}
+					{
+						currentUser
+						?
+						<Nav.Link href='/favorites'>View Favorites</Nav.Link>
+						:
+						null
+					}
+				</Nav>
 
-                    <Nav>
-                        <NavDropdown title="" id="collasible-nav-dropdown">
-                            {
-                                currentUser
-                                ?
-                                <NavDropdown.Item href="/add">Add a Watchable</NavDropdown.Item>
-                                :
-                                <NavDropdown.Item href='/watchables-list'>List</NavDropdown.Item>
-                            }
-                            <NavDropdown.Item href="/account">My account</NavDropdown.Item>
-
-                            <NavDropdown.Divider />
-
-                            {
-                                currentUser
-                                ?
-                                <NavDropdown.Item href="/logout">Logout</NavDropdown.Item>
-                                :
-                                <NavDropdown.Item href='/login'>Login</NavDropdown.Item>
-                            }
-
+				<Nav>
 
 
+					<NavDropdown>
+						{
+							currentUser
+							?
+							<NavDropdown.Item href='/add'>Add a Watchable</NavDropdown.Item>
+							:
+							null
+						}
+						{
+							currentUser
+							?
+							<NavDropdown.Item href='/account'>My account</NavDropdown.Item>
+							:
+							null
+						}
+						{
+							currentUser
+							?
+							<NavDropdown.Item href='/logout'>Logout</NavDropdown.Item>
+							:
+							<NavDropdown.Item href='/login'>Login</NavDropdown.Item>
+						}
+					</NavDropdown>
+					<Navbar.Text>
+						{currentUser ? `Hi, ${currentUser.username}` : 'Login'}
+					</Navbar.Text>
 
+				</Nav>
 
-
-                        </NavDropdown>
-
-                        <Navbar.Text>
-                            {currentUser ? `Hello, ${currentUser.username}` : 'Login'}
-                        </Navbar.Text>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-            {/* </Navbar> */}
-            </nav>
-        </div>
-    );
+			</Navbar.Collapse>
+		</Container>
+	</Navbar>
+	);
 };
 
 export default NavBar;
