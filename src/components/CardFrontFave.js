c63e8835c9797feb8f52e20011e9df310491755c
Chun Yi <8.5by11@gmail.com>
2022-04-11 12:33:16 -0400

    Fave page cards

---
+++ src/components/CardFrontFave.js
@@ -0,0 +1,15 @@
+import { Card } from 'react-bootstrap'
+
+import '../styles/Cards.css'
+
+function CardFrontFave({watchable}) {
+    return (
+        <div className='card-front'>
+            <Card style={{ width: '8rem' }}>
+                <Card.Img variant="top" src={watchable.poster_url} />
+            </Card>
+        </div>
+    );
+};
+
+export default CardFrontFave;
