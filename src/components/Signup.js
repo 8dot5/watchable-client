a481c7a4f40c46443214e482149135e362a43212
Chun Yi <8.5by11@gmail.com>
2022-04-11 17:50:05 -0400

    Refactored to support error handling and disabled button

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -5,15 +5,31 @@ import Errors from './Errors'
 import '../styles/SignupPage.css'
 
 function Signup({ handleLogin, errors }) {
-    const [state, setState] = useState({});
+    // const [state, setState] = useState({});
     const history = useHistory();
 
-    function onChange(e) {
-        setState({ ...state, [e.target.name]: e.target.value})
-    };
+    const [username, setUsername] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [passwordConfirmation, setPasswordConfirmation] = useState('')
 
     function handleSubmit(e) {
         e.preventDefault()
+        switch (true) {
+            case (formData.username.length <3):
+                alert('Need a longer username');
+                break;
+            case (formData.email.length <6):
+                alert('Need a longer email')
+                break;
+            case (formData.password.length <6):
+                alert('Enter a longer password')
+                break;
+            //TODO fix this
+            // case(!formData.password_confirmation === formData.password):
+            //     alert('Password does not match!')
+            //     break;
+            default:
 
         // calling the BE
         fetch('/users', {
@@ -21,7 +37,7 @@ function Signup({ handleLogin, errors }) {
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(state)
+            body: JSON.stringify(formData)
         })
         .then(res => res.json())
         // then, doing FE things
@@ -29,42 +45,53 @@ function Signup({ handleLogin, errors }) {
             // console.log(data, 'signup data')
             handleLogin(data)
             history.push('/watchables-list')
-        });
+        })}
     };
+
+    let formData = {
+        'username': username,
+        'email': email,
+        'password': password,
+        'password_confirmation': passwordConfirmation
+    }
+
+    let form = (
+        <form onSubmit={handleSubmit}>
+            <fieldset>
+                <legend>Sign up for a new Watchables Account!</legend>
+                <br/>
+                <label>Username
+                    <br/>
+                    <input onChange={(e) => setUsername(e.target.value)} className='input' id='username' type='text' name='username'/>
+                </label>
+                <br/>
+                <br/>
+                <label>Email
+                <br/>
+                    <input onChange={(e) => setEmail(e.target.value)}  className='input' type='text' name='email'/>
+                </label>
+                <br/>
+                <label>Password
+                <br/>
+                    <input onChange={(e) => setPassword(e.target.value)}  className='input' type='password' name='password'/>
+                </label>
+
+                <br/>
+                <label>Confirm password
+                <br/>
+                    <input onChange={(e) => setPasswordConfirmation(e.target.value)}  className='input' type='password' name='password_confirmation'/>
+                </label>
+                <br/>
+                <br/>
+                <input disabled={!username || !email || !password || !passwordConfirmation} id='submit' type='submit'></input>
+            </fieldset>
+        </form>
+    )
 
     return (
         <div className='signup-page'>
             <div className="page-title">
-            <form onSubmit={handleSubmit}>
-                <fieldset>
-                    <legend>Sign up for a new Watchables Account!</legend>
-                    <br/>
-                    <label>Username
-                        <br/>
-                        <input onChange={onChange} className='input' id='username' type='text' name='username'/>
-                    </label>
-                    <br/>
-                    <br/>
-                    <label>Email
-                    <br/>
-                        <input onChange={onChange} className='input' type='text' name='email'/>
-                    </label>
-                    <br/>
-                    <label>Password
-                    <br/>
-                        <input onChange={onChange} className='input' type='password' name='password'/>
-                    </label>
-
-                    <br/>
-                    <label>Confirm password
-                    <br/>
-                        <input onChange={onChange} className='input' type='password' name='password_confirmation'/>
-                    </label>
-                    <br/>
-                    <br/>
-                    <input id='submit' type='submit'></input>
-                </fieldset>
-            </form>
+            {form}
             </div>
             <Errors errors={errors} />
         </div>
