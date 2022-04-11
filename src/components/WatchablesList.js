083a1f23eb6e475736f236177424f219fa12b2ad
Chun Yi <8.5by11@gmail.com>
2022-04-11 12:37:35 -0400

    Removed console.log

--- src/components/WatchablesList.js
+++ src/components/WatchablesList.js
@@ -5,7 +5,6 @@ import '../styles/WatchablesList.css'
 function WatchablesList({ currentUser, userCategories, watchables, setWatchables, setWatchablesEdit, favorites, setFavorites }) {
 
     function renderCard() {
-        console.log(watchables, 'rendering the cards')
         return watchables.map((watchable) => {
             return <CardContainer key={watchable.id} watchable={watchable} setWatchablesEdit={setWatchablesEdit} watchables={watchables} setWatchables={setWatchables} favorites={favorites} setFavorites={setFavorites} userCategories={userCategories} />
         })
