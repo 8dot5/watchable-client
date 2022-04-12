4c940d150c382f93cd5fda784d013eca8dbb2763
Chun Yi <8.5by11@gmail.com>
2022-04-12 16:19:00 -0400

    WIP: static page

--- src/components/Static.js
+++ src/components/Static.js
@@ -2,13 +2,27 @@
 import '../styles/StaticPage.css'
 
 function Static(){
+
+    let src = 'https://m.psecn.photoshelter.com/img-get2/I0000oMj4CY3BzMw/fill=1375x1200/g=G00003tyCpRF.qOc/bg.jpg'
+
+    let style = {
+        // margin: "10px 5px 0px 5px",
+        // backgroundPosition: 'center',
+        // backgroundAttachment: 'fixed',
+        // backgroundRepeat: 'no-repeat',
+        // backgroundSize: 'cover',
+        opacity: 0.7,
+        width: '1500px'
+
+    }
+
+
     return (
-        <div className="static-page">
-            {/* <page className='page'>
+        <div className="static-background">
 
-            </page> */}
-            <div className='page-title'>Static Page</div>
-
+            <div className='static-body'>
+              <img src={src} style={style} />
+            </div>
 
         </div>
     )
