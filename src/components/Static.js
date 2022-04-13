a6a1703c77563a5f16e9a2ec4aff457bcf6531ee
Chun Yi <8.5by11@gmail.com>
2022-04-13 11:30:40 -0400

    Static page wip

--- src/components/Static.js
+++ src/components/Static.js
@@ -3,43 +3,25 @@ import '../styles/StaticPage.css'
 
 import Spinner from 'react-bootstrap/Spinner'
 
-
 function Static(){
-
-    let src = 'https://m.psecn.photoshelter.com/img-get2/I0000oMj4CY3BzMw/fill=1375x1200/g=G00003tyCpRF.qOc/bg.jpg'
-
-    // let src ='https://m.psecn.photoshelter.com/img-get/I0000oMj4CY3BzMw/s/1000?1649800949'
-
+    // let src = 'https://m.psecn.photoshelter.com/img-get2/I0000oMj4CY3BzMw/fill=2000x1125/g=G00003tyCpRF.qOc/bg.jpg'
+    let src = 'https://m.psecn.photoshelter.com/img-get2/I0000oMj4CY3BzMw/fit=1400/g=G00003tyCpRF.qOc/bg.jpg'
 
     let style = {
-        // margin: "10px 5px 0px 5px",
-        // backgroundPosition: 'center',
-        // backgroundAttachment: 'fixed',
-        // backgroundRepeat: 'no-repeat',
-        // backgroundSize: 'cover',
-
         opacity: 0.5,
-        // backgroundSize: 'auto',
         backgroundRepeat: 'no-repeat',
         backgroundPosition: 'center',
+        backgroundAttachment: 'fixed',
         backgroundSize: 'cover',
-        height: '100%',
-
+        height: '100%'
     }
 
-
     return (
-        <div className="static-background">
-
-            <div className='static-body'>
-              <img src={src} style={style} />
-
-              <Spinner animation='border' className='spinner' role='status'>
-                  <span className="visually-hidden">Loading...</span>
-              </Spinner>
-
-            </div>
-
+        <div className='static-body'>
+            <img src={src} style={style} />
+            <Spinner animation='border' className='spinner' role='status'>
+                <span className="visually-hidden">Loading...</span>
+            </Spinner>
         </div>
     )
 }
