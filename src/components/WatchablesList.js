5bcd3932612d840859e5f14c3fc42e895ddcc553
Chun Yi <8.5by11@gmail.com>
2022-04-13 11:41:28 -0400

    Commented out conditional rendering of the static image; causes ghost/flickering

--- src/components/WatchablesList.js
+++ src/components/WatchablesList.js
@@ -4,6 +4,8 @@ import '../styles/WatchablesList.css'
 
 function WatchablesList({ currentUser, userCategories, watchables, setWatchables, setWatchablesEdit, favorites, setFavorites }) {
 
+    // let src = 'https://m.psecn.photoshelter.com/img-get2/I0000P0l36xX6i_0/fit=1000x750/g=G00003tyCpRF.qOc/VRG-ILLO-4493-Spot-003-jpg.jpg'
+
     function renderCard() {
         return watchables.map((watchable) => {
             return <CardContainer key={watchable.id} watchable={watchable} setWatchablesEdit={setWatchablesEdit} watchables={watchables} setWatchables={setWatchables} favorites={favorites} setFavorites={setFavorites} userCategories={userCategories} />
@@ -20,8 +22,14 @@ function WatchablesList({ currentUser, userCategories, watchables, setWatchables
                 'Log in to see your List.'
             }
             </div>
-            <div className='watchables-grid'>
-                {renderCard()}
+            <div className='watchables-grid'>{
+                // currentUser
+                // ?
+                // renderCard()
+                // :
+                // <img src={src}/>
+                renderCard()
+            }
             </div>
         </div>
     )
