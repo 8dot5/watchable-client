781f5e19f76e6f8e2269f5724a946d7cdb2ef82a
Chun Yi <8.5by11@gmail.com>
2022-04-13 11:40:24 -0400

    Styling

--- src/components/CardFront.js
+++ src/components/CardFront.js
@@ -5,7 +5,7 @@ import '../styles/Cards.css'
 function CardFront({watchable}) {
     return (
         <div className='card-front'>
-            <Card style={{ width: '14rem' }}>
+            <Card style={{ width: '15rem' }}>
                 <Card.Img variant="top" style={{ objectFit: 'cover', height: '30vh'}} src={watchable.poster_url}/>
             </Card>
         </div>
